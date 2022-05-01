5740c2797e8d478b08f124c223770614025d7885
Erik5231 <rubes999@seznam.cz>
2022-05-01 04:08:34 +0200

    Update

--- app/src/main/java/spsmb/dinogame/MainActivity.java
+++ app/src/main/java/spsmb/dinogame/MainActivity.java
@@ -1,8 +1,11 @@
 package spsmb.dinogame;
 
 import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
     DisplayMetrics displayMetrics;
     @SuppressLint("StaticFieldLeak")
     public static TextView scoreText;
+    public static MediaPlayer mediaPlayer;
+    @SuppressLint("StaticFieldLeak")
+    public static Button startButton;
+    @SuppressLint("StaticFieldLeak")
+    public static View view;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +42,22 @@ public class MainActivity extends AppCompatActivity {
         Screen.width = displayMetrics.widthPixels;
         setContentView(R.layout.activity_main);
         scoreText = (TextView) findViewById(R.id.score);
+        startButton = (Button) findViewById(R.id.startButton);
+        view = (View) findViewById(R.id.gameView);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startButton.setVisibility(View.INVISIBLE);
+                GameView.speed = 15;
+                GameView.running = true;
+            }
+        });
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.picopark);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
+
     }
 }
